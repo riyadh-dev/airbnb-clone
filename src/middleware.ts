5fa05935656896bd2a3f051ddafe79fd98b8d0c7
Baatchia Riyadh <Baatchia_Riyadh@protonmail.com>
2023-04-29 20:14:50 +0100

    Specified some protected routes in the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-	matcher: ['/became-a-host/:step*', '/wishlist'],
+	matcher: ['/became-a-host/:step*', '/wishlist', '/trips', '/properties'],
 	pages: { signIn: '/' },
 };
